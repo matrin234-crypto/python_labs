5f56568140e6ccdb80fc34870bc85d242bbcfe58
matrin234-crypto <matrin234@gmail.com>
2026-09-11 11:09:50 +0300

    p

--- src/lab01/03_discount_vat.py
+++ src/lab01/03_discount_vat.py
@@ -1,11 +1,11 @@
-price = int(input("Цена: "))
-discount = int(input("Скидка: "))
-vat = int(input("НДС: "))
+price = float(input("Цена: "))
+discount = float(input("Скидка: "))
+vat = float(input("НДС: "))
 
 base = price * (1 - discount/100)
 vat_amount = base * (vat/100)
 total = base + vat_amount
 
-print(f"База после скидки: {base:>12.2f} ₽")
-print(f"НДС: {vat_amount:>12.2f} ₽")
-print(f"Итого к оплате: {total:>12.2f} ₽")
+print(f"{'База после скидки:':<20} {base:>12.2f} ₽")
+print(f"{'НДС:':<20} {vat_amount:>12.2f} ₽")
+print(f"{'Итого к оплате:':<20} {total:>12.2f} ₽")
